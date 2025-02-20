74a38328f7ff69e18a5cf8fc4a1dfe13d781e080
mr2cents <alexdibiasi1629@gmail.com>
2025-02-20 09:55:12 -0500

    Removed pull_data_str. Analysis handles dimensions correctly

--- webng/analysis/analysis.py
+++ webng/analysis/analysis.py
@@ -23,21 +23,21 @@ class weAnalysis:
         os.chdir(self.work_path)
         # Set names if we have them
         self.set_names(opts["pcoords"])
-        # we need to write assignment.py sometimes
-        self.pull_data_str = "import numpy as np\n"
-        self.pull_data_str += "def pull_data(n_iter, iter_group):\n"
-        self.pull_data_str += "    '''\n"
-        self.pull_data_str += "    This function reshapes the progress coordinate and\n"
-        self.pull_data_str += "    auxiliary data for each iteration and retuns it to\n"
-        self.pull_data_str += "    the tool.\n"
-        self.pull_data_str += "    '''\n"
-        self.pull_data_str += '    data_to_pull = np.loadtxt("data_to_pull.txt") - 1\n'
-        self.pull_data_str += "    d1, d2 = data_to_pull\n"
-        self.pull_data_str += (
-            "    pcoord  = iter_group['pcoord'][:,:,[int(d1),int(d2)]]\n"
-        )
-        self.pull_data_str += "    data = pcoord\n"
-        self.pull_data_str += "    return data\n"
+        # we need to write assignment.py sometimes, NOT ANYMORE
+        # self.pull_data_str = "import numpy as np\n"
+        # self.pull_data_str += "def pull_data(n_iter, iter_group):\n"
+        # self.pull_data_str += "    '''\n"
+        # self.pull_data_str += "    This function reshapes the progress coordinate and\n"
+        # self.pull_data_str += "    auxiliary data for each iteration and retuns it to\n"
+        # self.pull_data_str += "    the tool.\n"
+        # self.pull_data_str += "    '''\n"
+        # self.pull_data_str += '    data_to_pull = np.loadtxt("data_to_pull.txt") - 1\n'
+        # self.pull_data_str += "    d1, d2 = data_to_pull\n"
+        # self.pull_data_str += (
+        #     "    pcoord  = iter_group['pcoord'][:,:,[int(d1),int(d2)]]\n"
+        # )
+        # self.pull_data_str += "    data = pcoord\n"
+        # self.pull_data_str += "    return data\n"
 
     def _getd(self, dic, key, default=None, required=True):
         val = dic.get(key, default)
